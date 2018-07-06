09533af962367409c12efbe1987497977565dc69
MatFabi <matseb.fabiszewski@gmail.com>
2018-07-06 10:09:42 +0200

    random password creating method added

--- src/sample/controller/SignUpController.java
+++ src/sample/controller/SignUpController.java
@@ -1,15 +1,23 @@
 package sample.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.mysql.jdbc.StringUtils;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import sample.Database.DatabaseHandler;
+import javafx.stage.Stage;
+import sample.database.DatabaseHandler;
+import sample.model.User;
 
 public class SignUpController {
+
     @FXML
     private ResourceBundle resources;
 
@@ -23,33 +31,64 @@ public class SignUpController {
     private TextField signUpLastName;
 
     @FXML
-    private CheckBox signUpCheckBoxMale;
+    private TextField signUpTelNumber;
 
     @FXML
-    private TextField signUpLocation;
+    private TextField signUpPersonalId;
 
     @FXML
-    private PasswordField signUpPassword;
+    private TextArea signUpInfo;
+
+    @FXML
+    private TextField signUpZipCode;
 
     @FXML
     private Button signUpButton;
 
     @FXML
-    private TextField signUpUsername;
-
-    @FXML
-    private CheckBox signUpCheckBoxFeMale;
-
-    @FXML
     void initialize() {
+
         signUpButton.setOnAction(ev->{
+            createUser();
+        });
+
+
+    }
+    private void createUser(){
+        String name = signUpFirstName.getText();
+        String lastName = signUpLastName.getText();
+        String telNumber =signUpTelNumber.getText().trim().replace("-","");
+        String zipCode = signUpZipCode.getText().trim().replace("-","");
+        String info = signUpInfo.getText();
+        String personalId = signUpPersonalId.getText().trim();
+        String username;
+        String password;
+
+//        If we treat name, lastName, telNumber, personalId as a must, before we push them to our DB, we need to check if they are not empty,
+//        telNumber is 9 numbers length, and personal Id have 9 numbers
+
+        if(!name.equals("") && !lastName.equals("") && !telNumber.equals("")
+            && StringUtils.isStrictlyNumeric(telNumber) && telNumber.length()==9
+                && !personalId.equals("") && personalId.length()==11 && StringUtils.isStrictlyNumeric(personalId)) {
+
+            username=name+lastName;
+            password = randomPassword();
+
+            User user = new User(name, lastName, telNumber, personalId, zipCode, info, username,password);
             DatabaseHandler databaseHandler = new DatabaseHandler();
 
-            signUpButton.setOnAction(e->{
-                databaseHandler.signUpUser(signUpFirstName.getText(),signUpLastName.getText(),signUpUsername.getText()
-                        ,signUpPassword.getText(),signUpLocation.getText(), "Male");
-            });
-        });
+            databaseHandler.SignUpUser(user);
+        }
     }
-
+    private String randomPassword() {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+        char[] password = new char[8];
+        for (int i = 0; i < password.length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
 }
+
+
